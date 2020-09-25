509987eec50b14b8a8330c2c436c487222d91f46
Onyibe Godstime <godstimeonyibe2@gmail.com>
2020-09-25 16:17:50 +0100

    I added postman api that i used in fetching record

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,34 @@
-// fetch("https://covid-19-data.p.rapidapi.com/help/countries?format=json", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-// 		"x-rapidapi-key": "eb5f4d933fmsh1ed78c184783231p1c98d5jsn4fd0295cc79e"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response.json());
-// })
-// .catch(err => {
-// 	console.log(err);
-// });
+let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+let requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+fetch("https://covidnigeria.herokuapp.com/api", requestOptions)
+  .then(async response => {
+    const record = await response.json()
+    const data = record.data.states
+    for (let key in data) {
+      let obj = data[key];
+      console.log(obj.state,obj.confirmedCases,obj.discharged,obj.death)
+      
+    }
+    const totalConfirmedRecord = document.querySelector('#totalConfirmedRecord')
+    const totalDischargeRecord = document.querySelector('#totalDischargeRecord')
+    const totalDeathRecord = document.querySelector('#totalDeathRecord')
+    totalConfirmedRecord.innerHTML = record.data.totalConfirmedCases
+    totalConfirmedRecord.style.visibility = 'visible'
+    totalDischargeRecord.innerHTML = record.data.discharged
+    totalDischargeRecord.style.visibility = 'visible'
+    totalDeathRecord.innerHTML = record.data.death
+    totalDeathRecord.style.visibility = 'visible'
+    console.log(record.data.totalConfirmedCases)
+  })
+  .catch(error =>{
+    console.log(error)
+  })
 
 const dateBuilder = (d) => {
   const months = ['Jan','Feb','Mar','April','May','June','July','Aug','Sept','Oct','Nov','Dec']
