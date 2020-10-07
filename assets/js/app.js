fce7635f1ffc56be0d2d1f3c5bc154cd056b1723
Onyibe Godstime <godstimeonyibe2@gmail.com>
2020-10-07 17:08:57 +0100

    added favicon packages

--- assets/js/app.js
+++ assets/js/app.js
@@ -209,9 +209,9 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker
     .register('./sw.js')
     .then((registration) => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      console.log(`ServiceWorker registration successful with scope:, ${registration.scope}`);
     }, (error) => {
-      console.log('ServiceWorker registration failed: ', error);
+      console.log(`ServiceWorker registration failed:', ${error}`);
     })
   })
 }
