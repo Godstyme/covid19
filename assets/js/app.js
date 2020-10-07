6d9892b23a21be978a352cb0a27a53be05c1ba02
Onyibe Godstime <godstimeonyibe2@gmail.com>
2020-10-07 16:26:27 +0100

    added service worker to test

--- assets/js/app.js
+++ assets/js/app.js
@@ -205,13 +205,12 @@ Highcharts.mapChart('container', {
 
 // ==================== registration of service worker ============
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load',() => {
-    navigator.serviceWorker
-    .register('./sw.js')
-    .then((registration) => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, (error) => {
-      console.log('ServiceWorker registration failed: ', error);
-    })
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((reg) => {
+        console.log('Service worker registered.', reg.scope)
+      }, (err) => {
+        console.log('Service worker registration failed:', err)
+      })
   })
 }
